097f308cbe4f4ae117d7532fecd2b22f87e104d5
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2024-08-24 14:51:42 +0530

    Update basicDF.scala

--- sparksql/basicDF.scala
+++ sparksql/basicDF.scala
@@ -3,3 +3,7 @@
 // Create data
 val columns = Seq("language","users_count")
 val data = Seq(("Java", "20000"), ("Python", "100000"), ("Scala", "3000"))
+
+val dfFromRdd1 = rdd.toDF()
+dfFromRdd1.printSchema()
+dfFromRdd1.show()
