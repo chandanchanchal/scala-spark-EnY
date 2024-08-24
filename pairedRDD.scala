67aeea42c898f367b596f9cdc89433eb41fd186c
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2024-08-24 14:35:18 +0530

    Update pairedRDD.scala

--- pairedRDD.scala
+++ pairedRDD.scala
@@ -4,3 +4,13 @@
  val wordsRdd = rdd.flatMap(_.split(" "))
  val pairRDD = wordsRdd.map(f=>(f,1))
  pairRDD.foreach(println)
+
+//Applying distinct()
+pairRDD.distinct().foreach(println)
+//SortBykey on pairRDD
+println("sort by key ==>>")
+val sortRDD = pairRDD.sortByKey()
+sortRDD.foreach(println)
+
+val sortRDD = pairRDD.sortByKey()
+wordCount.foreach(println)
