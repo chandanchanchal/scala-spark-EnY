7046216e7507feef641e803d04b008b925ee8377
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2024-08-24 14:25:19 +0530

    Create pairedRDD.scala

---
+++ pairedRDD.scala
@@ -0,0 +1,6 @@
+ val rdd = spark.sparkContext.parallelize(
+      List("Germany India USA","USA India Russia","India Brazil Canada China")
+    )
+ val wordsRdd = rdd.flatMap(_.split(" "))
+ val pairRDD = wordsRdd.map(f=>(f,1))
+ pairRDD.foreach(println)
