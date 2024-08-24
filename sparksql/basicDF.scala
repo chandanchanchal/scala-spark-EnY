c0755a22ca726a922730ca2b7a4261b86fa0b472
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2024-08-24 16:00:56 +0530

    Update basicDF.scala

--- sparksql/basicDF.scala
+++ sparksql/basicDF.scala
@@ -78,3 +78,26 @@ val df_with_schema = spark.read.format("csv")
       .load("zipcodes.csv")
 df_with_schema.printSchema()
 df_with_schema.show(false)
+
+ val arrayStructureData = Seq(
+    Row(Row("James","","Smith"),List("Java","Scala","C++"),"OH","M"),
+    Row(Row("Anna","Rose",""),List("Spark","Java","C++"),"NY","F"),
+    Row(Row("Julia","","Williams"),List("CSharp","VB"),"OH","F"),
+    Row(Row("Maria","Anne","Jones"),List("CSharp","VB"),"NY","M"),
+    Row(Row("Jen","Mary","Brown"),List("CSharp","VB"),"NY","M"),
+    Row(Row("Mike","Mary","Williams"),List("Python","VB"),"OH","M")
+  )
+
+  val arrayStructureSchema = new StructType()
+    .add("name",new StructType()
+      .add("firstname",StringType)
+      .add("middlename",StringType)
+      .add("lastname",StringType))
+    .add("languages", ArrayType(StringType))
+    .add("state", StringType)
+    .add("gender", StringType)
+
+  val df = spark.createDataFrame(
+    spark.sparkContext.parallelize(arrayStructureData),arrayStructureSchema)
+  df.printSchema()
+  df.show()
