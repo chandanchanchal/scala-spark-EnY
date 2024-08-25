1308db2b7731c68d32801ce296018175f711e209
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2024-08-25 12:30:28 +0530

    Create Udf.scala

---
+++ Udf.scala
@@ -0,0 +1,8 @@
+import spark.implicits._
+val columns = Seq("Seqno","Quote")
+val data = Seq(("1", "Be the change that you wish to see in the world"),
+    ("2", "Everyone thinks of changing the world, but no one thinks of changing himself."),
+    ("3", "The purpose of our lives is to be happy.")
+  )
+val df = data.toDF(columns:_*)
+df.show(false)
