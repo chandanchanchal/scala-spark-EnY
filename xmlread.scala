f7714b53189d1d009fe9e02874c52d5e2a724f1c
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2024-08-25 14:27:21 +0530

    Update xmlread.scala

--- xmlread.scala
+++ xmlread.scala
@@ -1,14 +1,25 @@
- val schema = new StructType()
-      .add("_id",StringType)
-      .add("firstname",StringType)
-      .add("middlename",StringType)
-      .add("lastname",StringType)
-      .add("dob_year",StringType)
-      .add("dob_month",StringType)
-      .add("gender",StringType)
-      .add("salary",StringType)
-val df = spark.read
-  .option("rowTag", "book")
-  .schema(schema)
-  .xml("persons.xml")
-df.show()
+spark-shell --packages com.databricks:spark-xml_2.12:0.18.0
+import com.databricks.spark.xml._
+
+val df = spark.read.option("rowTag", "book").xml("/home/ansadmin/data/scala-spark-EnY/Data/books.xml")
+  
+  
+val selectedData = df.select("author", "_id")
+selectedData.write.option("rootTag", "books").option("rowTag", "book").xml("/home/ansadmin/data/scala-spark-EnY/Data/newbooks.xml")
+
+import org.apache.spark.sql.types.{StructType, StructField, StringType, DoubleType}
+
+val customSchema = StructType(Array(
+  StructField("_id", StringType, nullable = true),
+  StructField("author", StringType, nullable = true),
+  StructField("description", StringType, nullable = true),
+  StructField("genre", StringType, nullable = true),
+  StructField("price", DoubleType, nullable = true),
+  StructField("publish_date", StringType, nullable = true),
+  StructField("title", StringType, nullable = true)))
+
+
+val df = spark.read.option("rowTag", "book").schema(customSchema).xml("/home/ansadmin/data/scala-spark-EnY/Data/books.xml")
+
+val selectedData = df.select("author", "_id")
+selectedData.write.option("rootTag", "books").option("rowTag", "book").xml("/home/ansadmin/data/scala-spark-EnY/Data/newbooks1.xml")
