c25679e156d07a1bbb63a7b437a1bfb9180a5ca8
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2024-08-25 14:45:05 +0530

    Create usecase-1.scala

---
+++ usecase-1.scala
@@ -0,0 +1,4 @@
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.{Encoder, Encoders, DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions.{avg, sum}
+import java.sql.Date
