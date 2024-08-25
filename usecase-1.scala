16ee60981b07fd75cd284716f3a1feeb204a9da2
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2024-08-25 14:52:40 +0530

    Update usecase-1.scala

--- usecase-1.scala
+++ usecase-1.scala
@@ -2,3 +2,6 @@ import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{Encoder, Encoders, DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions.{avg, sum}
 import java.sql.Date
+
+
+def toDS[T <: Product: Encoder](df: DataFrame): Dataset[T] = df.as[T]
