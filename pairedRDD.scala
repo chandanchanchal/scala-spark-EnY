688b7f4bbf1509e3c0df670fe2647b7af3e19ff8
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-02-22 14:19:20 +0530

    Update pairedRDD.scala

--- pairedRDD.scala
+++ pairedRDD.scala
@@ -1,19 +1,4 @@
- val rdd = spark.sparkContext.parallelize(
-      List("Germany India USA","USA India Russia","India Brazil Canada China")
-    )
- val wordsRdd = rdd.flatMap(_.split(" "))
- val pairRDD = wordsRdd.map(f=>(f,1))
- pairRDD.foreach(println)
 
-//Applying distinct()
-pairRDD.distinct().foreach(println)
-//SortBykey on pairRDD
-println("sort by key ==>>")
-val sortRDD = pairRDD.sortByKey()
-sortRDD.foreach(println)
-
-val sortRDD = pairRDD.sortByKey()
-wordCount.foreach(println)
 
 val rdd = spark.sparkContext.parallelize(
       List("Germany India USA","USA India Russia","India Brazil Canada China")
